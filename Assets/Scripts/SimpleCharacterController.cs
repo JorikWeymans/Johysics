6c939469da65e63ae5817dc94d7f39af6b26954c
Jorik Weymans <jorikweymans@hotmail.be>
2021-01-04 22:11:48 +0100

    simplecharcontroller now uses rididbody to move isntead of charcont

--- Assets/Scripts/SimpleCharacterController.cs
+++ Assets/Scripts/SimpleCharacterController.cs
@@ -5,27 +5,33 @@ using UnityEngine;
 
 namespace Jorik
 {
-    [RequireComponent(typeof(CharacterController))]
+    [RequireComponent(typeof(Rigidbody))]
 	public sealed class SimpleCharacterController : MonoBehaviour
     {
         [SerializeField] private float _Speed = 10.0f;
-
-        private CharacterController _Cont = null;
+        [SerializeField] private float _RotationSpeed = 2.0f;
+        private Rigidbody _Cont = null;
         private Camera _MainCamera = null;
 
         private void Awake()
         {
-            _Cont = GetComponent<CharacterController>();
+            _Cont = GetComponent<Rigidbody>();
             _MainCamera = Camera.main;
         }
 
         private void FixedUpdate()
         {
-            Vector2 xValue = _MainCamera.transform.right.normalized * Input.GetAxis("Horizontal") * _Speed;
-            Vector2 yValue = _MainCamera.transform.up.normalized * Input.GetAxis("Vertical") * _Speed;
+            Vector3 xValue = _MainCamera.transform.right.normalized * Input.GetAxis("Horizontal") * _Speed;
+            Vector3 yValue = _MainCamera.transform.up.normalized * Input.GetAxis("Vertical") * _Speed;
 
-            _Cont.Move(xValue  + yValue);
+            Vector3 newPos =  transform.position + xValue + yValue;
+
+            _Cont.MovePosition(newPos);
+
             
+
+
+
         }
 	}
 }
